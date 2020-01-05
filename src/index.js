aa6588b8ae479b14557842d1f26f3f9d0a1cf933
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-01-05 01:22:25 -0500

    added tachyon styling to cards and put the cards onto the page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import React from 'react';
 //Does the DOM manipulation for us
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
 //I want the react dom package to use the function render and this render will render in the first parameter
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+                <CardList robots={ robots }/>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
