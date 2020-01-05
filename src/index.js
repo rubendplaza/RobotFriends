13f91bbb3da2aa56982d7070cf5e6fc61c3dbc8c
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-01-04 23:46:26 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+//This syntax works because react has webpack under the hood that does the bundling for us.
 import React from 'react';
+//Does the DOM manipulation for us
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//I want the react dom package to use the function render and this render will render in the first parameter
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
