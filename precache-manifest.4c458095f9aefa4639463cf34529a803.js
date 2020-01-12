e8f19c350269eb0e97a5c87fd03f60d6c4c39a95
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-01-11 20:11:18 -0500

    Updates

---
+++ precache-manifest.4c458095f9aefa4639463cf34529a803.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47de1f68f469720e9bcc88fbd44bd45",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "3de70804e3661970565e",
+    "url": "/RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "25deb9db867f24aefdc3",
+    "url": "/RobotFriends/static/css/main.d3480591.chunk.css"
+  },
+  {
+    "revision": "3de70804e3661970565e",
+    "url": "/RobotFriends/static/js/2.007a6dc4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RobotFriends/static/js/2.007a6dc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25deb9db867f24aefdc3",
+    "url": "/RobotFriends/static/js/main.6eed4650.chunk.js"
+  },
+  {
+    "revision": "8427ac07013efc64a5d8",
+    "url": "/RobotFriends/static/js/runtime-main.2973dfde.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "304eddde048016a82ac070a64d581bea",
+    "url": "/RobotFriends/static/media/angela.304eddde.jpeg"
+  },
+  {
+    "revision": "1e7fca2df630ec5b7a8963331ad00993",
+    "url": "/RobotFriends/static/media/darlene.1e7fca2d.jpeg"
+  },
+  {
+    "revision": "ce1b67ca69ab31e38c6c4b507b27b479",
+    "url": "/RobotFriends/static/media/dom.ce1b67ca.jpeg"
+  },
+  {
+    "revision": "b39ca1cfa9175c487a255cc0c5984ef4",
+    "url": "/RobotFriends/static/media/elliot.b39ca1cf.jpeg"
+  },
+  {
+    "revision": "891afd6dab082f157698aac1b4a8fdf5",
+    "url": "/RobotFriends/static/media/gideon.891afd6d.jpeg"
+  },
+  {
+    "revision": "a6db3c021768b1aec8d40329ea17482e",
+    "url": "/RobotFriends/static/media/joanna.a6db3c02.jpeg"
+  },
+  {
+    "revision": "0317fe74fa85aa5a9657d4eddb4dae75",
+    "url": "/RobotFriends/static/media/krista.0317fe74.jpeg"
+  },
+  {
+    "revision": "5f6401bd7aff4c0667f80141af44629d",
+    "url": "/RobotFriends/static/media/mrrobot.5f6401bd.jpeg"
+  },
+  {
+    "revision": "a647bf46296e28ae746cbffb19e7f02e",
+    "url": "/RobotFriends/static/media/price.a647bf46.jpeg"
+  },
+  {
+    "revision": "6365ea234b23fc51356bf3353bb93e3f",
+    "url": "/RobotFriends/static/media/scott.6365ea23.jpeg"
+  },
+  {
+    "revision": "d3f3a4fb95b637f3aba0e002b0482c3f",
+    "url": "/RobotFriends/static/media/shayla.d3f3a4fb.jpeg"
+  },
+  {
+    "revision": "147d49b11fc8d3eeac4df8b48ec09b72",
+    "url": "/RobotFriends/static/media/susan.147d49b1.jpeg"
+  },
+  {
+    "revision": "efc9b00bdffb09f817ce49a8f3621c3a",
+    "url": "/RobotFriends/static/media/tyrell.efc9b00b.jpeg"
+  },
+  {
+    "revision": "ea2f25551e597b562fc6b909fe1efac0",
+    "url": "/RobotFriends/static/media/vera.ea2f2555.jpeg"
+  },
+  {
+    "revision": "2ecf2ea32424eb5025b7450bcc32bfe2",
+    "url": "/RobotFriends/static/media/whiterose.2ecf2ea3.jpeg"
+  }
+]);
